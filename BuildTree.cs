9970a5c727e15839273d68638e3172ddfb22f35a
Gustavo Armenta <gustavo.armenta@gmail.com>
2015-06-03 15:31:42 -0700

    first commit

---
+++ BuildTree.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+
+namespace BuildOptimization
+{
+    public class BuildTree
+    {
+        public string RootPath { get; set; }
+        public Dictionary<string, string> Mapping { get; set; }
+
+        private Dictionary<string, int> referencedProjects;
+        private Dictionary<string, string> lowercaseProjectMap;
+        private Output output;
+
+        public BuildTree()
+        {
+            this.Mapping = new Dictionary<string, string>();
+        }
+        public Output Analyze()
+        {
+            output = new Output();
+
+            referencedProjects = new Dictionary<string, int>();
+            lowercaseProjectMap = new Dictionary<string, string>();
+            var files = Directory.GetFiles(this.RootPath, "*.csproj", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                output.Projects.Add(file);
+                referencedProjects.Add(file.ToLowerInvariant(), 0);
+                lowercaseProjectMap.Add(file.ToLowerInvariant(), file);
+            }
+
+            TraverseDirsProj();
+
+            foreach (var project in referencedProjects)
+            {
+                if (project.Value == 0)
+                {
+                    var dir = Path.GetDirectoryName(project.Key);
+                    bool shouldKeep = false;
+                    foreach(var project2 in referencedProjects)
+                    {
+                        var dir2 = Path.GetDirectoryName(project2.Key);
+                        if(project2.Value > 0 && string.Equals(dir, dir2))
+                        {
+                            shouldKeep = true;
+                            continue;
+                        }
+                    }
+
+                    if (!shouldKeep)
+                    {
+                        output.ProjectsNotReferenced.Add(lowercaseProjectMap[project.Key]);
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        private void TraverseDirsProj()
+        {
+            var dirsItems = Directory.GetFiles(this.RootPath, "dirs.proj", SearchOption.TopDirectoryOnly);
+            var queue = new Queue<string>();
+            queue.Enqueue(Path.Combine(this.RootPath, "dirs.proj").ToLowerInvariant());
+
+            while (queue.Count > 0)
+            {
+                var dirs = queue.Dequeue();
+                
+                XmlDocument doc = new XmlDocument();
+                doc.Load(dirs);
+                XmlNode root = doc.DocumentElement;
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+                nsmgr.AddNamespace("a", "http://schemas.microsoft.com/developer/msbuild/2003");
+                XmlNodeList nodeList = root.SelectNodes("//a:ProjectFile", nsmgr);
+
+                foreach (var item in nodeList)
+                {
+                    var projectFile = ((XmlNode)item).Attributes["Include"].Value;
+                    projectFile = projectFile.Replace("*", "");
+                    projectFile = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(dirs), projectFile));
+                    projectFile = projectFile.ToLowerInvariant();
+
+                    if (string.Equals("dirs.proj", Path.GetFileName(projectFile)))
+                    {
+                        queue.Enqueue(projectFile);
+                    }
+
+                    if (!projectFile.EndsWith(".csproj"))
+                    {
+                        continue;
+                    }
+
+                    if (referencedProjects.ContainsKey(projectFile))
+                    {
+                        referencedProjects[projectFile] += 1;
+                        TraverseProjectReferences(projectFile);
+                    }
+                    else
+                    {
+                        output.DirsProjectFileNotFound.Add(string.Join(",", dirs, projectFile));
+                    }
+                }
+            }
+        }
+
+        private void TraverseProjectReferences(string projectFile)
+        {
+            if (!projectFile.EndsWith(".csproj"))
+            {
+                return;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(projectFile);
+            XmlNode root = doc.DocumentElement;
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+            nsmgr.AddNamespace("a", "http://schemas.microsoft.com/developer/msbuild/2003");
+            XmlNodeList nodeList = root.SelectNodes("//a:ProjectReference", nsmgr);
+
+            foreach (var item in nodeList)
+            {
+                var projectReference = ((XmlNode)item).Attributes["Include"].Value;
+                projectReference = projectReference.ToLowerInvariant();
+
+                if (!projectReference.EndsWith(".csproj"))
+                {
+                    continue;
+                }
+
+                if (projectReference.StartsWith("$"))
+                {
+                    var mapKey = projectReference.Substring(0, projectReference.IndexOf(')') + 1);
+                    if (!this.Mapping.ContainsKey(mapKey))
+                    {
+                        throw new Exception(string.Format("missing entry on BuildTree.Mapping: {0}", projectReference));
+                    }
+
+                    var mapValue = this.Mapping[mapKey.ToLowerInvariant()];
+                    projectReference = projectReference.Replace(mapKey, mapValue);
+                }
+                else
+                {
+                    projectReference = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(projectFile), projectReference));
+                }
+
+                projectReference = projectReference.ToLowerInvariant();
+                if (File.Exists(projectReference))
+                {
+                    if (referencedProjects.ContainsKey(projectReference))
+                    {
+                        referencedProjects[projectReference] += 1;
+                        if (referencedProjects[projectReference] == 1)
+                        {
+                            TraverseProjectReferences(projectReference);
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception(string.Format("not found in referenceProjects: {0}", projectReference));
+                    }
+                }
+                else
+                {
+                    output.ProjectReferencesNotFound.Add(string.Join(",", projectFile, projectReference));
+                }
+            }
+        }
+    }
+
+    public class Output
+    {
+        public List<string> Projects { get; set; }
+        public List<string> ProjectsNotReferenced { get; set; }
+        public List<string> DirsProjectFileNotFound { get; set; }
+        public List<string> ProjectReferencesNotFound { get; set; }
+
+        public Output()
+        {
+            this.DirsProjectFileNotFound = new List<string>();
+            this.ProjectReferencesNotFound = new List<string>();
+            this.Projects = new List<string>();
+            this.ProjectsNotReferenced = new List<string>();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BuildOptimization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buildTree = new BuildTree();
+            buildTree.RootPath = @"D:\one\AD\MSODS\Core\src\";
+            buildTree.Mapping.Add("$(administrationpath)", Path.Combine(buildTree.RootPath, @"dev\om\administration"));
+            buildTree.Mapping.Add("$(cacheservicepath)", Path.Combine(buildTree.RootPath, @"dev\cacheservice"));
+            buildTree.Mapping.Add("$(coexistencepath)", Path.Combine(buildTree.RootPath, @"dev\coexistence"));
+            buildTree.Mapping.Add("$(commonpath)", Path.Combine(buildTree.RootPath, @"dev\common"));
+            buildTree.Mapping.Add("$(configurationpath)", Path.Combine(buildTree.RootPath, @"dev\configuration"));
+            buildTree.Mapping.Add("$(deploymentpath)", Path.Combine(buildTree.RootPath, @"dev\mgmtsys\deployment"));
+            buildTree.Mapping.Add("$(devpath)", Path.Combine(buildTree.RootPath, "dev"));
+            buildTree.Mapping.Add("$(dms)", Path.Combine(buildTree.RootPath, @"dev\mgmtsys\dms"));
+            buildTree.Mapping.Add("$(dspath)", Path.Combine(buildTree.RootPath, @"dev\ds"));
+            buildTree.Mapping.Add("$(functionalitymanagementpath)", Path.Combine(buildTree.RootPath, @"dev\common\functionalitymanagement"));
+            buildTree.Mapping.Add("$(identitypath)", Path.Combine(buildTree.RootPath, @"dev\identity"));
+            buildTree.Mapping.Add("$(inetroot)", @"d:\one\ad\msods\core");
+            buildTree.Mapping.Add("$(pushendpointspath)", Path.Combine(buildTree.RootPath, @"dev\pushendpoints"));
+            buildTree.Mapping.Add("$(reportingpath)", Path.Combine(buildTree.RootPath, @"dev\mgmtsys\Reporting"));
+            buildTree.Mapping.Add("$(restpath)", Path.Combine(buildTree.RootPath, @"dev\restservices"));
+            buildTree.Mapping.Add("$(testpath)", Path.Combine(buildTree.RootPath, @"test"));
+            buildTree.Mapping.Add("$(windowsfabricpath)", Path.Combine(buildTree.RootPath, @"dev\windowsfabric"));
+            buildTree.Mapping.Add("$(workflowspath)", Path.Combine(buildTree.RootPath, @"dev\om\workflows"));
+
+            var output = buildTree.Analyze();
+
+            var excludeDirectories = new List<string>();
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\AzureExpressDeployment\TopologyCreator\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\coexistence\dirsyncclient");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\coexistence\DirSyncClientV2\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\ds\InstantOn\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\LiveSiteWorker\MsodsLiveSiteClient\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\mgmtsys\synthetictransaction\monitoringstx");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\test\ds\datapopulator\dscorecommon\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\ds\lib\ServiceInstanceMoveClient\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\ds\SelectiveSync\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\ds\tasks\DSTasks.SampleHost\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\ds\webservices\SyncService.SampleHost\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\Notifications\Examples\SubscriptionStore.ExtensionExample\");
+            excludeDirectories.Add(@"D:\one\AD\MSODS\Core\src\dev\Notifications\Store\SubscriptionStore.ExchangeExtension.ConsoleTests\");
+
+            Console.WriteLine("Total Projects: {0}", output.Projects.Count);
+            Console.WriteLine("Projects not referenced: {0}", output.ProjectsNotReferenced.Count);
+            Console.WriteLine("dirs.proj file references a project not found: {0}", output.DirsProjectFileNotFound.Count);
+            Console.WriteLine("*.csproj file references a project not found: {0}", output.ProjectReferencesNotFound.Count);
+
+            Console.WriteLine();
+            Console.WriteLine("Exclude directories:");
+            foreach (var excludeDirectory in excludeDirectories)
+            {
+                Console.WriteLine(excludeDirectory);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Projects not referenced:");
+            foreach (var projectNotReferenced in output.ProjectsNotReferenced)
+            {
+                bool shouldKeep = false;
+                foreach (var excludeDirectory in excludeDirectories)
+                {
+                    if (projectNotReferenced.StartsWith(excludeDirectory))
+                    {
+                        shouldKeep = true;
+                        break;
+                    }
+                }
+
+                if (!shouldKeep)
+                {
+                    Console.WriteLine(projectNotReferenced);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Commands to delete directories:");
+            foreach (var projectNotReferenced in output.ProjectsNotReferenced)
+            {
+                bool shouldKeep = false;
+                foreach (var excludeDirectory in excludeDirectories)
+                {
+                    if (projectNotReferenced.StartsWith(excludeDirectory))
+                    {
+                        shouldKeep = true;
+                        break;
+                    }
+                }
+
+                if (!shouldKeep)
+                {
+                    Console.WriteLine("git rm -r \"{0}\"", Path.GetDirectoryName(projectNotReferenced));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("dirs.proj file references a project not found:");
+            foreach (var dirsProjectFileNotFound in output.DirsProjectFileNotFound)
+            {
+                Console.WriteLine(dirsProjectFileNotFound);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("*.csproj file references a project not found:");
+            foreach (var projectReferencesNotFound in output.ProjectReferencesNotFound)
+            {
+                Console.WriteLine(projectReferencesNotFound);
+            }
+        }
+    }
+}
